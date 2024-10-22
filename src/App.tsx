a9f719dc4c3bb056203035a3a7b6cde7c37dcee2
Danil-Golubev <dokers190rus@mail.ru>
2024-10-22 16:11:54 +0300

    fix first commit eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React, {useEffect, useState} from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import { Html5Qrcode } from 'html5-qrcode';
 
 function App() {
   const [isEnabled, setEnabled] = useState(false)
 const [res, setRes] = useState('')
-const [res2, setRes2] = useState('')
+// const [res2, setRes2] = useState('')
   useEffect(()=>{
 
     const config = {fps:10, qrbox:{width:200, height:200}}
